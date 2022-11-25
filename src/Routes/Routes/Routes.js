8c337538fa482e4b04f396c4383972621977ab87
Joti <tanjilajoti@gmail.com>
2022-11-25 17:03:07 +0600

    add nav & footer

---
+++ src/Routes/Routes/Routes.js
@@ -0,0 +1,59 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../../Layout/Main";
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main></Main>,
+        children: [
+            {
+                path: '/',
+
+                // element: <Home></Home>
+            },
+            {
+                path: '/login',
+                // element: <Login></Login>
+            },
+            {
+                path: '/signup',
+                // element: <SignUp></SignUp>
+            },
+            {
+                path: '/blogs',
+                // element: <Appointment></Appointment>
+            }
+        ]
+    },
+    // {
+    //     path: '/dashboard',
+    //     element: <PrivateRoute><DashboardLayout></DashboardLayout></PrivateRoute>,
+    //     children: [
+    //         {
+    //             path: '/dashboard',
+    //             element: <MyAppointment></MyAppointment>
+    //         },
+    //         {
+    //             path: '/dashboard/allusers',
+    //             element: <AdminRoute><AllUsers></AllUsers></AdminRoute>
+    //         },
+    //         {
+    //             path: '/dashboard/adddoctor',
+    //             element: <AdminRoute><AddDoctor></AddDoctor></AdminRoute>
+    //         },
+    //         {
+    //             path: '/dashboard/managedoctors',
+    //             element: <AdminRoute><ManageDoctors></ManageDoctors></AdminRoute>
+    //         },
+    //         {
+    //             path: '/dashboard/payment/:id',
+
+    //             element: <AdminRoute><Payment></Payment></AdminRoute>,
+    //             loader: ({ params }) => fetch(`http://localhost:5000/bookings/${params.id}`)
+    //         },
+    //     ]
+
+    // }
+])
+
+export default router;
